e26aad7070fd8be2e99712ef955907c155c7eba1
gdulab <gdula.bartlomiej@gmail.com>
2023-06-18 16:44:32 +0200

    ADD Animations

--- src/components/views/Hero/Hero.js
+++ src/components/views/Hero/Hero.js
@@ -1,20 +1,26 @@
-import styles from './Hero.module.scss'
+import styles from './Hero.module.scss';
+import { motion } from 'framer-motion';
 
 
 //icons
-import html5icon from "../../../icons/html-5.png"
-import css3icon from "../../../icons/css-3.png"
-import bootstrapicon from "../../../icons/bootstrap.png"
-import jsicon from "../../../icons/js.png"
-import reacticon from "../../../icons/react.png"
-import linkedinicon from "../../../icons/linkedin.png"
-import githubicon from "../../../icons/github.png"
+import html5icon from "../../../icons/html-5.png";
+import css3icon from "../../../icons/css-3.png";
+import bootstrapicon from "../../../icons/bootstrap.png";
+import jsicon from "../../../icons/js.png";
+import reacticon from "../../../icons/react.png";
+import linkedinicon from "../../../icons/linkedin.png";
+import githubicon from "../../../icons/github.png";
+
 
 
 const Hero = () => {
     return (
         <div className={styles.heroWrapper}>
-            <div className={styles.heroLeft}>
+            <motion.div className={styles.heroLeft}
+                initial={{ opacity: 0, x: -500 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ type: "tween", duration: 0.5 }}
+            >
                 <div className={styles.heroLeftContentWrapper}>
                     <div className={styles.heroText}>
                         <h2 className={styles.hello}>Cześć!</h2>
@@ -35,15 +41,22 @@ const Hero = () => {
                         <img src={reacticon} alt="react icon"></img>
                     </div>
                 </div>
-            </div>
+            </motion.div>
             <div className={styles.heroRight}>
-                <div className={styles.photo}></div>
-                <div className={styles.socials}>
+                <motion.div className={styles.photo}
+                    initial={{ opacity: 0, y: -500 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ type: "tween", duration: 0.5, delay: 0.2 }}>
+                </motion.div>
+                <motion.div className={styles.socials}
+                    initial={{ opacity: 0, x: 500 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "tween", duration: 0.5, delay: 0.3 }}>
                     <div className={styles.linksWrapper}>
                         <a href='https://www.linkedin.com/in/bart%C5%82omiej-gdula/'><img src={linkedinicon} alt="linkedin icon"></img></a>
                         <a href='https://github.com/gdulab'><img src={githubicon} alt="github icon"></img></a>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
 
